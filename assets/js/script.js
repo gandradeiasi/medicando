e44d70b0f48a2c09de22f2d6013799724d164732
Gabriel Iasi <gandrade.iasi@gmail.com>
2019-08-29 17:01:15 -0300

    Tela de remédios e farmácias

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
     //esconde frames
-    $("#tela-login, #tela-registro, #tela-medicamentos, #tela-localizacao").hide();
+    $("#tela-login, #tela-registro, #tela-medicamentos, #tela-farmacias").hide();
 
     //Click
     $(".btn-home").on("click",function() {toggleHome("home")});
     $(".btn-medicamentos").on("click",function() {toggleHome("medicamentos")});
-    $(".btn-localizacao").on("click",function(){toggleHome("localizacao")});
+    $(".btn-farmacias").on("click",function(){toggleHome("farmacias")});
 });
 
 //Funçoes globais
@@ -21,11 +21,11 @@ function toggleLogin(tela) {
 }
 
 function toggleHome(tela) {
-    $("#tela-home, #tela-medicamentos, #tela-localizacao").hide();
+    $("#tela-home, #tela-medicamentos, #tela-farmacias").hide();
 
     switch(tela) {
         case "home": $("#tela-home").show(); break;
         case "medicamentos": $("#tela-medicamentos").show(); break;
-        case "localizacao": $("#tela-localizacao").show(); break;
+        case "farmacias": $("#tela-farmacias").show(); break;
     }
 }
